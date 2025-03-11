eae74b174f8da23b415a7e022bbb0a699b47598a
keed001 <kenethk621@gmail.com>
2025-03-12 00:50:26 +0300

    it is time to sleep

---
+++ practice.py
@@ -0,0 +1,20 @@
+x = int (3)
+y = int (7)
+z = bool (False)
+
+#print (type (x))
+#print (type (y))
+#print (type (z))
+
+m = f = k = "car"
+#print (m)
+#print (f)
+#print (k)
+
+cars = ["Toyota", "Benz", "Mazda"]
+#print (a)
+#print (b)
+#print (c)
+#print (cars)
+
+print (x + y)
